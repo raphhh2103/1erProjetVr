7308303c614c016445b5704087bc093d4cf6e7ff
raphaelk <raphaelk@live.be>
2018-03-15 11:26:22 +0100

    Initial commit

---
+++ Assets/EnterTriggerTeleport.cs
@@ -0,0 +1,34 @@
+﻿
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnterTriggerTeleport : MonoBehaviour
+{
+
+    public Transform respawn;
+
+    private void OnTriggerEnter(Collider other)
+    {
+
+        if (other.tag == "Player")
+        {
+            Debug.DrawLine(other.transform.position, other.transform.position + new Vector3(0, 50000, 0), Color.red, 30);
+
+            other.transform.position = respawn.position;
+
+            Rigidbody rig = GetComponent<Rigidbody>();
+            if (rig != null)
+            {
+
+                rig.velocity = Vector3.zero;
+            }
+
+
+            Debug.Log("Player enter trigger", this.gameObject);
+
+        }
+    }
+}
+
---
+++ Assets/script/LimitOfTeleport.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OutOfLimitTeleport : MonoBehaviour
+{
+
+    public int heightToTeleport = -10;
+    public Transform objectToMove;
+    public Transform whereToRespwan;
+    public Rigidbody rigidbodyOfCharater;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+        if (objectToMove.position.y < heightToTeleport)
+        {
+
+            objectToMove.position = whereToRespwan.position;
+            rigidbodyOfCharater.velocity = Vector3.zero;
+
+
+
+        }
+    }
+}
+
+
+
